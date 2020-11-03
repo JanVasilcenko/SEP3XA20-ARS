12ad682edf0788223934abe7cebabcedf225a027
Jan Vasilcenko <jan.vasilcenko@gnj.cz>
2020-11-03 13:56:42 +0100

    Client model

---
+++ Client/Client/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class User
+    {
+        private string userType;
+        private string email;
+        private string password;
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+        private int phoneNumber;
+
+        public User(string userType, string email, string password, string firstName, string lastName, DateTime dateOfBirth, int phoneNumber) 
+        {
+            this.userType = userType;
+            this.email = email;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dateOfBirth = dateOfBirth;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
