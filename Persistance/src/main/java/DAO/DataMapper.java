2770a2dcae5a1d054bb444909622e9894e62fb6d
Jan Vasilcenko <jan.vasilcenko@gnj.cz>
2020-11-13 17:49:48 +0100

    cleaning

--- Persistance/src/main/java/DAO/DataMapper.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface DataMapper<T>
-{
-  T create(ResultSet rs)throws SQLException;
-}
--- Persistance/src/main/java/DAO/DepartureDAO.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-import Shared.Departure;
-
-public interface DepartureDAO
-{
-  void addDeparture(Departure newDeparture,int FlightID);
-}
