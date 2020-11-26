f919a48df1c3bc9194c00dc70819029daa3bee90
Jan Vasilcenko <jan.vasilcenko@gnj.cz>
2020-11-26 13:30:22 +0100

    Cleaning...

--- Persistance/src/main/java/DAO/PassengerDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import Shared.Passenger;
-
-public interface PassengerDAO
-{
-  Passenger getPassenger(int passportNum);
-  Passenger RegisterPassenger(Passenger newPassenger, String email);
-}
