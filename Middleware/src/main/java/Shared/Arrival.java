3ab4fc7bc7201d102e485c2fc457f627793478e6
Niczk <popal.nicolas@gmail.com>
2020-12-05 18:10:36 +0100

    Rebirth

---
+++ Middleware/src/main/java/Shared/Arrival.java
@@ -0,0 +1,30 @@
+package Shared;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class Arrival implements Serializable
+{
+  public Timestamp arrivalTime;
+  public String fromWhere;
+  public int flightID;
+
+  public Arrival(Timestamp arrivalTime,String fromWhere,int flightID)
+  {
+    this.arrivalTime = arrivalTime;
+    this.fromWhere = fromWhere;
+    this.flightID = flightID;
+  }
+
+  public int getFlightID()
+  {
+    return flightID;
+  }
+
+  @Override public String toString()
+  {
+    return "Arrival{" + "arrivalTime=" + arrivalTime + ", fromWhere='"
+        + fromWhere + '\'' + ", flightID=" + flightID + '}';
+  }
+}
